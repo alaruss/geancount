e8f4486aa4f4ccce9cbc63f1d29434d214c20be2
Nikita Larin <nikita.larin@gameloft.com>
2024-05-17 22:09:30 +0200

    Add check ledger subcommand

--- cmd/main.go
+++ cmd/main.go
@@ -14,29 +14,57 @@ func init() {
 
 }
 
+func printBalances(cCtx *cli.Context) error {
+	filename := cCtx.Args().Get(0)
+	ledger := geancount.NewLedger()
+	err := ledger.LoadFile(filename)
+	if err != nil {
+		return err
+	}
+	ls, err := ledger.GetState()
+	if err != nil {
+		return err
+	}
+	err = ledger.PrintBalances(ls)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkLedger(cCtx *cli.Context) error {
+	filename := cCtx.Args().Get(0)
+	ledger := geancount.NewLedger()
+	err := ledger.LoadFile(filename)
+	if err != nil {
+		return err
+	}
+	_, err = ledger.GetState()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // CreateCLI creates  CLI interface
 func CreateCLI() {
 	app := &cli.App{
-		Name:  "geancount",
-		Usage: "Loads beancount file and prints balances",
-		Action: func(cCtx *cli.Context) error {
-			filename := cCtx.Args().Get(0)
-			ledger := geancount.NewLedger()
-			err := ledger.LoadFile(filename)
-			if err != nil {
-				return err
-			}
-			ls, err := ledger.GetState()
-			if err != nil {
-				return err
-			}
-			err = ledger.PrintBalances(ls)
-			if err != nil {
-				return err
-			}
-			return nil
+		Name:   "geancount",
+		Usage:  "A utility to process beancount files. By default print balances",
+		Action: printBalances,
+		Commands: []*cli.Command{
+			{
+				Name:    "balances",
+				Aliases: []string{"bal"},
+				Usage:   "Prints balances",
+				Action:  printBalances,
+			},
+			{
+				Name:   "check",
+				Usage:  "Check ledger",
+				Action: checkLedger,
+			},
 		},
-		Commands: []*cli.Command{},
 	}
 
 	if err := app.Run(os.Args); err != nil {
